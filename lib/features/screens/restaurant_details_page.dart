20431e463b2025c56735022f641a2f2b210766aa
Abrahan-Eagle <thehackeroffire@gmail.com>
2025-06-28 16:00:18 -0400

    restaurant logo items

--- lib/features/screens/restaurant_details_page.dart
+++ lib/features/screens/restaurant_details_page.dart
@@ -165,8 +165,8 @@ class _RestaurantDetailsPageState extends State<RestaurantDetailsPage> {
                           Row(
                             children: [
                               Container(
-                                width: 80,
-                                height: 80,
+                                width: 100,
+                                height: 100,
                                 decoration: BoxDecoration(
                                   color: Colors.white,
                                   borderRadius: BorderRadius.circular(12),
