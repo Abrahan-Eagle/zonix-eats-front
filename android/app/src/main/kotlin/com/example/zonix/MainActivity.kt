028226fc0f425f5ded62c6f60aae15690135eb4a
Abrahan <thehackeroffire@gmail.com>
2026-02-09 13:36:02 -0400

    chore: Android config, pubspec y .vscode/extensions.json (sin cambios de onboarding)

--- android/app/src/main/kotlin/com/example/zonix/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/zonix/MainActivity.kt
@@ -1,5 +1,22 @@
 package com.example.zonix
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    /**
+     * Evita NullPointerException cuando el selector de fotos (u otra Activity)
+     * devuelve un Intent con extras null. El plugin image_picker llama
+     * getExtras().getString(...) y falla si getExtras() es null.
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val safeData = when {
+            data == null -> Intent().putExtras(Bundle())
+            data.extras == null -> Intent().putExtras(Bundle()).apply { this.data = data.data }
+            else -> data
+        }
+        super.onActivityResult(requestCode, resultCode, safeData)
+    }
+}
